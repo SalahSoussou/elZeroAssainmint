015545182bbb0d17f84a68c44ff5d77681953676
SalahSoussou <kaka79702@gmail.com>
2022-09-12 10:41:56 +0100

    don

--- dom.js
+++ dom.js
@@ -22,10 +22,35 @@ heder.append(logo, list);
 // header styling =================================
 document.body.style.cssText = "margin: 0;padding: 0;";
 heder.style.cssText = `height:40px; display: flex;justify-content: space-between;align-items: center;padding: 10px;`;
-list.style.cssText = `display: flex;margin: 5px 0;justify-content: space-between;align-items: center;flex-gap:gap; list-style: none;`;
+list.style.cssText = `display: flex; margin: 5px 0;justify-content: space-between;align-items: center;flex-gap:gap; list-style: none;`;
 li1.style.cssText = "margin: 0 10px;";
 li2.style.cssText = "margin: 0 10px;";
 li3.style.cssText = "margin: 0 10px;";
 li4.style.cssText = "margin: 0 10px;";
+
+// contener =================================
+let box, productN, text;
+
+for (let i = 1; i <= 15; i++) {
+  box = document.createElement("div");
+  productN = document.createElement("span");
+  text = document.createTextNode("Product");
+  productN.innerText = i;
+  box.append(productN, text);
+
+  box.style.cssText = `width:30%`;
+
+  conteaner.appendChild(box);
+}
+
+conteaner.style.cssText = `display: flex;    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    align-items: center;
+    padding: 15px;
+    justify-content: center;`;
+
+text = document.createTextNode("Product");
+footer.append(text);
 
 document.body.append(heder, conteaner, footer);
