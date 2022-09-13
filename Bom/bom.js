6f2152b47469e25a4f14f46e73aa094a3cbed0bf
SalahSoussou <kaka79702@gmail.com>
2022-09-13 19:49:58 +0100

    ok

--- Bom/bom.js
+++ Bom/bom.js
@@ -1,1 +1,40 @@
-console.log("hello");
+/*
+
+*/
+setTimeout(
+  (user) => {
+    console.log("time out !" + user);
+  },
+  2000,
+  "salah"
+);
+
+let handler = setInterval(() => {
+  num.innerHTML -= 1;
+  if (num.innerHTML === "0") {
+    clearInterval(handler);
+  }
+}, 1000);
+
+/*
+-location
+--herf get/set 
+--host
+--hash
+--protocol
+--reload()
+--replace()
+--assign()
+ */
+console.log(location);
+// location.href = "https://salahsoussou.tk/";
+
+// location.replace("https://google.com");   go to google insted of this page
+
+// location.assign("https://google.com");   go to google without clearing history
+// http://127.0.0.1:5500/
+
+/*
+--window.open()
+--window.close()
+*/
