561ad619ee47d34e337151d38e66498f80d87d8b
SalahSoussou <kaka79702@gmail.com>
2022-09-14 20:17:32 +0100

    ok

--- Bom/tasc app/app.js
+++ Bom/tasc app/app.js
@@ -1,14 +1,24 @@
 let addA = document.getElementById("add");
 let container = document.getElementById("container");
 
+let data = [];
 addA.onclick = () => {
+  let id = (Math.random() * 123 * 356).toFixed(15);
   let task = document.createElement("div");
   let delet = document.createElement("button");
   delet.innerText = "delet";
+  delet.id = id;
+
   if (text.value !== "") {
-    let tasc = document.createTextNode(text.value);
-    task.append(tasc, delet);
-    container.appendChild(task);
+    data.push({ titel: text.value, id: id });
+    localStorage.tacks = JSON.stringify(data);
     text.value = "";
   } else return;
+
+  if (data !== []) {
+    data.forEach((e) => {
+      task.append(e.titel, delet);
+    });
+  }
+  container.append(task);
 };
