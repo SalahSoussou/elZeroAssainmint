383ea703b494e1dadeab556d97f6740a1ca35a14
SalahSoussou <kaka79702@gmail.com>
2022-09-14 13:26:01 +0100

    ok

--- Bom/tasc app/js/app.js
+++ Bom/tasc app/js/app.js
@@ -1,2 +1,14 @@
-"use strict";
-console.log('first');
+let addA = document.getElementById("add");
+let container = document.getElementById("container");
+
+addA.onclick = () => {
+  let task = document.createElement("div");
+  let delet = document.createElement("button");
+  delet.innerText = "delet";
+  if (text.value !== "") {
+    let tasc = document.createTextNode(text.value);
+    task.append(tasc, delet);
+    container.appendChild(task);
+    text.value = "";
+  } else return;
+};
