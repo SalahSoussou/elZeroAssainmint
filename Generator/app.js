6cb9be4d75b082976e160f2796bb8c21e4ac1044
SalahSoussou <kaka79702@gmail.com>
2022-09-21 10:04:15 +0100

    ok

--- Generator/app.js
+++ Generator/app.js
@@ -3,20 +3,49 @@
 */
 
 function* genNumbers() {
-  yield 1;
-  alert("yield number 1");
+  yield "hello";
+  console.log("yield number 1");
   yield 2;
-  alert("yield number 2");
+  console.log("yield number 2");
   yield 3;
   yield 4;
   yield 5;
 }
 
 let genereated = genNumbers();
-
+// console.log(genNumbers);
 console.log(typeof genereated);
 console.log(genereated);
 
 console.log(genereated.next());
 console.log(genereated.next());
 console.log(genereated.next());
+
+//++++++++++++++++++++++++++++++++++
+
+function* numbers() {
+  yield 1;
+  yield 2;
+}
+function* litters() {
+  yield "A";
+  yield "B";
+}
+
+function* genAll() {
+  yield* numbers();
+  yield* litters();
+  yield* [7, 8, 9];
+}
+
+let all = genAll();
+console.log("#".repeat(44));
+
+console.log(all.next());
+console.log(all.next());
+console.log(all.next());
+console.log(all.next());
+console.log(all.next());
+console.log(all.next());
+console.log(all.next());
+console.log("#".repeat(44));
