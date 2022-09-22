551f24e84dac4cf720aa2c77418e46b94fc92b7f
SalahSoussou <kaka79702@gmail.com>
2022-09-22 19:24:34 +0100

    ok

--- Json/app.js
+++ Json/app.js
@@ -97,3 +97,75 @@ const promes = new Promise((resolve, reject) => {
   })
   .catch((noWiner) => console.log(noWiner))
   .finally(console.log("promis is done"));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+const getData = (url) => {
+  return new Promise((resolve, reject) => {
+    let myReq = new XMLHttpRequest();
+    myReq.onload = () => {
+      if (myReq.readyState === 4 && myReq.status === 200) {
+        resolve((jsData = JSON.parse(myReq.responseText)));
+      }
+      reject(Error("no data"));
+    };
+
+    myReq.open("GET", url);
+    myReq.send();
+  });
+};
+
+getData("http://localhost:8000/quotes")
+  .then((data) => {
+    data.length = 3;
+    return data;
+  })
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      let text = document.createElement("div");
+      text.innerHTML = `〃${data[i].quote}〃</br><p><i>-${data[i].author}</i></p>`;
+      document.body.appendChild(text);
+    }
+  })
+  .catch((rej) => {
+    console.log(rej);
+  });
+
+///////====================
+fetch("http://localhost:8000/quotes")
+  .then((data) => {
+    return data.json();
+  })
+  .then((data) => {
+    data.length = 3;
+    for (let i = 0; i < data.length; i++) {
+      let text = document.createElement("div");
+      text.innerHTML = `<p><i>-${data[i].author} -- fetch</i></p> `;
+      document.body.appendChild(text);
+    }
+  })
+  .catch((rej) => {
+    console.log(Error("no data"));
+  });
+
+/*
+  Promise.all()
+  Promise.allSettled()
+  Promise.race()
+*/
+
+//////////========= Async   ===========
+
+async function getUsers() {
+  let users = ["hi"];
+  if (users.length > 0) {
+    return "Users found";
+  } else {
+    throw new Error("no user found");
+  }
+}
+
+getUsers().then(
+  (res) => console.log(res),
+  (rej) => console.log(rej)
+);
